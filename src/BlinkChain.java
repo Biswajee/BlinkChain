9c720661b6ffc59607559ab8b8f78537cea09a15
Biswajee <roy.biswajeet161@gmail.com>
2019-02-17 20:32:08 +0530

    that's all for litmus test !

--- src/BlinkChain.java
+++ src/BlinkChain.java
@@ -2,10 +2,11 @@ import java.util.ArrayList;
 import com.google.gson.GsonBuilder;
 public class BlinkChain {
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
-
+    public static int difficulty = 5;
     public static Boolean isChainValid() {
         Block currentBlock;
         Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 
         //loop through blockchain to check hashes:
         for(int i=1; i < blockchain.size(); i++) {
@@ -21,6 +22,11 @@ public class BlinkChain {
                 System.out.println("Previous Hashes not equal");
                 return false;
             }
+            //check if hash is solved
+            if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
+                System.out.println("This block hasn't been mined");
+                return false;
+            }
         }
         return true;
     }
@@ -29,8 +35,16 @@ public class BlinkChain {
     public static void main(String[] args) {
         //add our blocks to the blockchain ArrayList:
         blockchain.add(new Block("Hi im the first block", "0"));
+        System.out.println("Trying to mine block 1...");
+        blockchain.get(0).mineBlock(difficulty);
         blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to mine block 2...");
+        blockchain.get(1).mineBlock(difficulty);
         blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to mine block 3...");
+        blockchain.get(2).mineBlock(difficulty);
+
+        System.out.println("\nBlockchain is valid : " + isChainValid());
 
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
         System.out.println(blockchainJson);
